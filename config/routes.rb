5c7557c201d09ca98612bafbaf14d219349d74c4
Domenic Murtari <domenic.murtari@gmail.com>
2014-06-23 13:59:31 -0600

    Routing and navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  root 'static_pages#home'
+  get '/help' => 'static_pages#help', as: :help
+  #get 'static_pages/help'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
