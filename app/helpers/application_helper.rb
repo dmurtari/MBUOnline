204144d1f57d36a53bec0d963144f71b1f563e38
Domenic Murtari <domenic.murtari@gmail.com>
2014-06-23 13:35:38 -0600

    Basic static page structure with Bootstrap
    Still need to add content to pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return the full title of the page
+  def full_title(page_title)
+    base_title = "MBU Online"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
