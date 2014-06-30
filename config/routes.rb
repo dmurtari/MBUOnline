05304a465816914c9c573188e906ef6a01ae1bd4
Domenic Murtari <domenic.murtari@gmail.com>
2014-06-30 12:19:16 -0600

    User sign up, and associated tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get '/help' => 'static_pages#help', as: :help
-  #get 'static_pages/help'
-
+  get '/signup' => 'users#new', as: :signup
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Users", :type => :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
-      expect(response.status).to be(200)
-    end
+  subject { page }
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.firstname) }
+    it { should have_title("#{user.firstname} #{user.lastname}") }
+  end
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
   end
 end
