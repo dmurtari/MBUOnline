7fd8c8eadd60d429af9a16200810bf1b28cd666f
Domenic Murtari <domenic.murtari@gmail.com>
2014-06-30 11:18:15 -0600

    Cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
 
   # Actions to perform before saving to database
-  before_save { self.email = email.downcase }
-  
+  before_save { email.downcase! }
+
   # Validations on new user creation
   validates :firstname, :lastname, :phone, presence: true
   validates :firstname, :lastname, length: {
     maximum: 40,
     too_long: "First or last name cannot be more than 40 characters"
   }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, 
             format: { with: VALID_EMAIL_REGEX }
 
