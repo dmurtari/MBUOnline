5ffacc156329b1e7a49b019a783a89ab8baa5db7
Domenic Murtari <domenic.murtari@gmail.com>
2014-06-30 11:16:45 -0600

    Modified schema to support passwords

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630154907) do
+ActiveRecord::Schema.define(version: 20140630164925) do
 
   create_table "users", force: true do |t|
     t.string   "firstname"
     t.string   "lastname"
     t.integer  "troop"
-    t.string   "distric"
+    t.string   "district"
     t.string   "phone"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
