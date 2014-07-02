24cc3c42bc4771c53945c75a33c798a729d1f087
Domenic Murtari <domenic.murtari@gmail.com>
2014-07-02 14:43:19 -0600

    Added errors and requiring information for scouts

--- app/models/scout.rb
+++ app/models/scout.rb
@@ -1,4 +1,5 @@
 class Scout < ActiveRecord::Base
   belongs_to :user
-  validates :user_id, presence: true
+  validates :user_id, :firstname, :lastname, :dob, :emergency_relation,
+            :emergency_name, :emergency_phone, presence: true
 end
