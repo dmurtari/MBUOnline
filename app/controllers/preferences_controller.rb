1d544d45240e838e2ce4ddab23b62e1b21c80090
Domenic Murtari <domenic.murtari@gmail.com>
2014-07-17 10:21:14 -0600

    Cleaned up handling adding and deleting course preferences

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -5,14 +5,14 @@ class PreferencesController < ApplicationController
     @course = Course.find(params[:scout][:preferences])
     @scout = current_user.scouts.find_by(id: params[:scout_id])
     @scout.add_preference!(@course)
-    redirect_to current_user
+    redirect_to edit_scout_path(@scout)
   end
 
   def destroy
     @course = Course.find(Preference.find(params[:id]).course_id)
     @scout = Scout.find(Preference.find(params[:id]).scout_id)
     @scout.remove_preference!(@course)
-    redirect_to current_user
+    redirect_to edit_scout_path(@scout)
   end
 
 end
