d9b990e533648b7c1c262a70ad0aafe27cf0143a
Domenic Murtari <domenic.murtari@gmail.com>
2014-07-23 14:11:05 -0600

    Fixed preferences not deleting when scout or course is destroyed

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ActiveRecord::Base
   has_many :records
   has_many :scouts, through: :records
 
-  has_many :preferences
+  has_many :preferences, dependent: :destroy
   has_many :requesting_scouts, through: :preferences, source: :scout
   
   validates :room, presence: true
--- app/models/scout.rb
+++ app/models/scout.rb
@@ -2,13 +2,15 @@ class Scout < ActiveRecord::Base
   has_many :records
   has_many :preferences
   has_many :courses, through: :records
-  has_many :preferred_courses, through: :preferences, source: :course
+  has_many :preferred_courses, through: :preferences, source: :course, dependent: :destroy
 
   before_save :reformat_phone
 
   belongs_to :user
   validates :user_id, :firstname, :lastname, :dob, :emergency_relation,
             :emergency_name, presence: true
+
+  validate :too_many_preferences, on: :update
 
   VALID_PHONE_REGEX = /\d{3}.*\d{3}.*\d{4}/
   validates :emergency_phone, presence: true, format: { with: VALID_PHONE_REGEX }
@@ -30,5 +32,11 @@ class Scout < ActiveRecord::Base
     def reformat_phone
       phone_match = /(\d{3}).*(\d{3}).*(\d{4})/.match(self.emergency_phone.to_s)
       self.emergency_phone = "(#{phone_match[1]}) #{phone_match[2]}-#{phone_match[3]}"
+    end
+
+    def too_many_preferences
+      if self.preferences.count > 2
+        errors.add(:scout, "Can't have more than 6 preferred courses")
+      end
     end
 end
