7f597106e831139cc352e0ebfd08ed73e0f238a6
Domenic Murtari <domenic.murtari@gmail.com>
2015-03-01 11:24:34 -0700

    Assign event to be most current event before saving record or preference

--- app/models/record.rb
+++ app/models/record.rb
@@ -3,7 +3,15 @@ class Record < ActiveRecord::Base
   belongs_to :course
   belongs_to :event
 
+  before_save :assign_event
+
   validates :scout, :course, presence: true
   validates :period, uniqueness: { scope: :scout_id,
     message: "Sorry, assignment for this period already exists"}
+
+  def assign_event
+    event = Event.where(current: true).last
+    self.event = event
+  end
+  
 end
